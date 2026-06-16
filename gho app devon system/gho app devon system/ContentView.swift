c10f7c63aa34d169b928341b059eed5eda09a1d4
gho system <igdevoncorneluis@gmail.com>
2026-06-16 18:43:49 +0200

    Restore working super admin dashboard

---
+++ gho app devon system/gho app devon system/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  gho app devon system
+//
+//  Created by Devon on 2026/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView(
---
+++ gho app devon system/gho app devon system/gho_app_devon_systemApp.swift
@@ -0,0 +1,17 @@
+//
+//  gho_app_devon_systemApp.swift
+//  gho app devon system
+//
+//  Created by Devon on 2026/06/16.
+//
+
+import SwiftUI
+
+@main
+struct gho_app_devon_systemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
